2bf7376adc68622e99a0a0d1aba33a0a471ef732
Villager A <100194237+habataku@users.noreply.github.com>
2022-12-30 14:07:16 +0900

    Update uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/u/',
-    bare: '/bear/',
+    bare: 'https://www.winterwaffles.ml/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
